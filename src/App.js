4af96e35eb61e30c296ecd0f21ddf0540cbcd1b0
Sanatan Kisku <sanatankisku@gmail.com>
2025-03-14 16:36:34 +0530

    Fixed case sensitivity and import issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GetReport from "./GetReport";
-import ReportCard from "./ReportCard";
+import GetReport from "./components/GetReport";  // Correct path
+import ReportCard from "./components/ReportCard";  // Correct path
 
 function App() {
   return (
