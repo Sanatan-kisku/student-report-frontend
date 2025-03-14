4236c2f59939f39c6612d6859d26eb8d00341453
Sanatan Kisku <sanatankisku@gmail.com>
2025-03-14 17:49:44 +0530

    Fixed routes

--- src/components/GetReport.js
+++ src/components/GetReport.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./styles/GetReport.css"; // Ensure this file exists
+import "../styles/GetReport.css"; // Ensure this file exists
 
 const classMapping = {
   6: "VI",
