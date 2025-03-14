8db381f23260bf0ad3d11a480fefe8aefe091171
Sanatan Kisku <sanatankisku@gmail.com>
2025-03-14 16:45:05 +0530

    Fixed react-router-dom issue

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import "./styles/ReportCard.css"; // If inside styles folder
+import "../styles/ReportCard.css"; // If inside styles folder
 
 
 const ReportCard = () => {
