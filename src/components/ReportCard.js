510cfa640ae9e518c02e5c1334baf57ff6870a48
Sanatan Kisku <sanatankisku@gmail.com>
2025-03-18 07:24:19 +0530

    Fixed useState & useEffect import issue

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
